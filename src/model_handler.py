db6a4be86eef40fa458c704c9d93f5e53e7f514c
nbonahoo <142938346+nbonahoo@users.noreply.github.com>
2025-09-14 12:53:46 -0400

    Added import, moving project to ECEprog

--- src/model_handler.py
+++ src/model_handler.py
@@ -1,6 +1,9 @@
-from huggingface_hub import HfApi, ModelCard
+from huggingface_hub import HfApi, ModelCard, snapshot_download
 
 api = HfApi()
 # or:
 info = api.model_info("LLM360/K2-Think")
 print(info.id, info.sha, info.cardData)
+
+# Model Downloading
+
