6de3cefaa91f7da23b16352df273a1897a1b05be
Noah Bonahoom <nbonahoo@purdue.edu>
2025-09-14 13:17:05 -0400

    working on testing model downloads

--- src/model_handler.py
+++ src/model_handler.py
@@ -7,3 +7,14 @@ print(info.id, info.sha, info.cardData)
 
 # Model Downloading
 
+def modelDownload(id):
+    ###  WARNING CODE FROM CHAT GPT, REQUIRES FURTHER TESTING
+    model_path = snapshot_download(repo_id="distilbert-base-uncased")
+
+    print(f"Model downloaded to: {model_path}")
+    ###
+    
+    
+if __name__ == "__main__":
+    modelPath = "LLM360/K2-Think"
+    modelDownload(id = modelPath)
