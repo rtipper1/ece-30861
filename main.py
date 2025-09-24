5bf1b8903b2a39542173c92fd985a9e7c1e02629
Rory Tippery <rtipper@purdue.edu>
2025-09-24 15:23:26 -0400

    changed run test scrpit to try to fix coverage issue

--- main.py
+++ main.py
@@ -47,15 +47,6 @@ metrics = [
         
 def main(argv=None):
     cli_args = parse_args(argv)
-    
-    # NOTE: install is also handled by top-level `run` script.
-    # Keeping here for backwards compatibility when running `python main.py install`.
-    if cli_args.command == 'install':
-        print("Installing dependencies from requirements.txt...")
-        subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
-
-    if cli_args.command == 'test':
-        run_tests()
 
     if cli_args.command == 'process':
         lines = parse_url_file(cli_args.url_file)
